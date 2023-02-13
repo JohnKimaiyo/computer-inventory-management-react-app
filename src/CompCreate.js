50285f3ee5f70b047a030e03878bfaed3d3279e0
JOHN KIMAIYO <kimaiyojohn6@gmail.com>
2023-02-13 20:39:12 +0300

    inital commit

---
+++ src/CompCreate.js
@@ -0,0 +1,152 @@
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+
+const Compreate = () => {
+  const [model, modelchange] = useState("");
+  const [name, namechange] = useState("");
+  const [processor, processorchange] = useState("");
+  const [ram, ramchange] = useState("");
+  const [display, displaychange] = useState("");
+  const [graphics, graphicschange] = useState("");
+  const [warranty, warrantychange] = useState("");
+
+  const navigate = useNavigate();
+
+  const handlesubmit = (e) => {
+    e.preventDefault();
+    const compdata = {
+      name,
+      model,
+      processor,
+      ram,
+      display,
+      graphics,
+      warranty,
+    };
+
+    fetch("http://localhost:8000/comp", {
+      method: "POST",
+      headers: { "content-type": "application/json" },
+      body: JSON.stringify(empdata),
+    })
+      .then((res) => {
+        alert("Saved successfully.");
+        navigate("/");
+      })
+      .catch((err) => {
+        console.log(err.message);
+      });
+  };
+
+  return (
+    <div>
+      <div className="row">
+        <div className="offset-lg-3 col-lg-6">
+          <form className="container" onSubmit={handlesubmit}>
+            <div className="card" style={{ textAlign: "left" }}>
+              <div className="card-title">
+                <h2>Computer Create</h2>
+              </div>
+              <div className="card-body">
+                <div className="row">
+                  <div className="col-lg-12">
+                    <div className="form-group">
+                      <label>MODEL</label>
+                      <input
+                        value={id}
+                        disabled="disabled"
+                        className="form-control"
+                      ></input>
+                    </div>
+                  </div>
+
+                  <div className="col-lg-12">
+                    <div className="form-group">
+                      <label>Name</label>
+                      <input
+                        required
+                        value={name}
+                        onMouseDown={(e) => valchange(true)}
+                        onChange={(e) => namechange(e.target.value)}
+                        className="form-control"
+                      ></input>
+                      {name.length == 0 && validation && (
+                        <span className="text-danger">Enter the name</span>
+                      )}
+                    </div>
+                  </div>
+
+                  <div className="col-lg-12">
+                    <div className="form-group">
+                      <label>RAM</label>
+                      <input
+                        value={email}
+                        onChange={(e) => emailchange(e.target.value)}
+                        className="form-control"
+                      ></input>
+                    </div>
+                  </div>
+
+                  <div className="col-lg-12">
+                    <div className="form-group">
+                      <label>DISPLAY</label>
+                      <input
+                        value={phone}
+                        onChange={(e) => phonechange(e.target.value)}
+                        className="form-control"
+                      ></input>
+                    </div>
+                  </div>
+                  <div className="col-lg-12">
+                    <div className="form-group">
+                      <label>GRAPHICS</label>
+                      <input
+                        value={phone}
+                        onChange={(e) => phonechange(e.target.value)}
+                        className="form-control"
+                      ></input>
+                    </div>
+                  </div>
+                  <div className="col-lg-12">
+                    <div className="form-group">
+                      <label>WARRANTY</label>
+                      <input
+                        value={phone}
+                        onChange={(e) => phonechange(e.target.value)}
+                        className="form-control"
+                      ></input>
+                    </div>
+                  </div>
+                  <div className="col-lg-12">
+                    <div className="form-check">
+                      <input
+                        checked={active}
+                        onChange={(e) => activechange(e.target.checked)}
+                        type="checkbox"
+                        className="form-check-input"
+                      ></input>
+                      <label className="form-check-label">Is Active</label>
+                    </div>
+                  </div>
+                  <div className="col-lg-12">
+                    <div className="form-group">
+                      <button className="btn btn-success" type="submit">
+                        Save
+                      </button>
+                      <Link to="/" className="btn btn-danger">
+                        Back
+                      </Link>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default EmpCreate;
+Footer;
